aa71bcef66861723588ee939533c18a03ad0308e
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-02-22 17:32:30 +0100

    Duplicated alx-react-app directory, started styling the header and userprofile components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+const style = {backgroundColor: "yellow", color: "red"}
+function MainContent() {
+    return (
+        <main>
+            <p style={style}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
