2479964db3a58a0d865c3ef8996fa1ec3059aeff
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-02-23 23:40:06 +0100

    Added styling to Navbar component

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div >
-            <ul style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '15px 8%', gap: '15px' }}>
+            <ul style={{ backgroundColor: 'grey', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '15px 8%', gap: '15px' }}>
                 <li style={{ listStyleType: 'none' }}><Link to="/">Home</Link></li>
                 <li style={{ listStyleType: 'none' }}><Link to="/about">About</Link></li>
                 <li style={{ listStyleType: 'none' }}><Link to="/services">Services</Link></li>
