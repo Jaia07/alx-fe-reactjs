f4480930998180bcf08a98ee3698ea4a75e18599
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-03-15 22:30:09 +0100

    Corrected user profile component to include md:p-8

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 sm:p-4 md:p8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <div className="bg-gray-100 p-8 sm:p-4 md:p-8 max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
         <img
         className="rounded-full w-36 h-36 sm:w-24 h-24 md:w-36 h-36 hover:scale-110 transition-transform duration-300 ease-in-out" 
         src="https://via.placeholder.com/150" 
