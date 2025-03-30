e9f2f3eff0de749c721b69f957095e536652d8cb
Joseph Adebanjo <josephadebanjo4@gmail.com>
2025-03-30 09:29:38 +0100

    Included fetchUserData in githubServices file

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,36 @@
+// import axios from 'axios';
+
+// const GITHUB_API_BASE_URL = 'https://api.github.com';
+
+// export const advancedSearchUsers = async (queryParams) => {
+//   try {
+//     const response = await axios.get(`${GITHUB_API_BASE_URL}/search/users`, {
+//       params: queryParams,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error during advanced user search:", error);
+//     throw error;
+//   }
+// };
+
+
 import axios from 'axios';
 
 const GITHUB_API_BASE_URL = 'https://api.github.com';
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_BASE_URL}/users/${username}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error('User not found');
+    }
+    console.error("Error fetching user data:", error);
+    throw error;
+  }
+};
 
 export const advancedSearchUsers = async (queryParams) => {
   try {
